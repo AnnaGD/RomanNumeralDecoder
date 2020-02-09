1edee0cb36768dee7599fd26625eafbaf420e20a
Anna Goncharova <anna.gonch.liaison@gmail.com>
2020-02-09 14:07:45 -0600

    Final code for review. Cleaned up code and removed unecessary else if statement. Updated comments in code.

--- romanNumeralDecoder.js
+++ romanNumeralDecoder.js
@@ -20,9 +20,12 @@ function romanNumeralDecoder(romanNumeral) {
 	for (let i = 0; i < romanNumeral.length; i++) {
 
     //In the the 1st loop the romanNumeral at i = 0, is the value of the 1st index in the string. In this instance we're calling "IV" and the zero index is "I". The for loop will return the value of romanMap['I'] = 1. 
+    
     //In the 2nd loop the romanNumeral at i = 1, is the value of the 2nd index in the string, in this isntance the for-loop returns romanMap['V'] = 5. 
+
     // In the if statment romanMap['I'] is less then romanMap['V'] then the decodedValue needs to be updated to the greater value minus the smaller value, which in this case romanMap['V'] - romanMap['I'] is 4 and the decodedValue is assigned a new value of 4.
-    // The else statment allows the decodedValue to be updated to romanMap[] in the case that the romanMap[romanNumeral[i+1]] being less than the romanMap[romanNumeral[i]]
+    
+    // The else statment handles all other cases. 
 		if (romanMap[romanNumeral[i]] < romanMap[romanNumeral[i + 1]]) {
             // console.log ('if romanMap : ' + romanMap[romanNumeral[i + 1]])
 
@@ -32,12 +35,7 @@ function romanNumeralDecoder(romanNumeral) {
             // return decodedValue;
             // break
             }   
-            else  if (romanMap[romanNumeral[i]] >= romanMap[romanNumeral[i + 1]]) {
-            console.log('else if romanMap : ' + romanMap[romanNumeral[i + 1]])
-
-			decodedValue += romanMap[romanNumeral[i]]
-            // console.log('elseif decodedValue :' + decodedValue)
-            }
+            
             else {
                 decodedValue += romanMap[romanNumeral[i]]
             //   console.log('else decodedValue :' + decodedValue)
@@ -46,6 +44,6 @@ function romanNumeralDecoder(romanNumeral) {
 	return decodedValue;
 }
 
-console.log('CD :' + romanNumeralDecoder("CD"))
+console.log('IXX :' + romanNumeralDecoder("IXX"))
 
 console.log(romanNumeralDecoder("V") === 5);
