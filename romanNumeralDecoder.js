57d42ee3197e611884f15149a367f0f1a45f669c
Anna Goncharova <anna.gonch.liaison@gmail.com>
2020-02-09 09:16:44 -0600

    saving progress fromrepl.it from previous night. I think I'm on the right track, but IV is returning9. If I remove the else statement 3 returns 0.

--- romanNumeralDecoder.js
+++ romanNumeralDecoder.js
@@ -1,22 +1,25 @@
-function romanNumeralDecoder(romanNumeral){
-    const romanMap = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-    };
+function romanNumeralDecoder(romanNumeral) {
+	const romanMap = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500
+	};
 
-    let decodedValue = 0;
+	let decodedValue = 0;
 
-    for (let i =0; i < romanNumeral.length; i++)
-    {
-        decodedValue += romanMap[romanNumeral[i]];
-    }   
-    return decodedValue;
+	for (let i = 0; i < romanNumeral.length; i++) {
+		if (romanMap[romanNumeral[i]] < romanMap[romanNumeral[i + 1]]) {
+			decodedValue += romanMap[romanNumeral[i + 1]] - romanMap[romanNumeral[i]];
+			}   else {
+			  decodedValue +=romanMap [romanNumeral[i]]
+		}
+	}
+	return decodedValue;
 }
 
-console.log(romanNumeralDecoder('III'));
+console.log(romanNumeralDecoder("III"));
 
-console.log(romanNumeralDecoder('III') ===3);
+console.log(romanNumeralDecoder("III") === 3);
